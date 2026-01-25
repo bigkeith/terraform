16754068c8420acc7dec5d8d0fb67560fee214b8
bigkeith <bigkeith@2256north.net>
2026-01-25 17:10:46 -0600

    adding the lab node

--- k8s-cluster/main.tf
+++ k8s-cluster/main.tf
@@ -28,3 +28,18 @@ module "workers" {
   template_name = "ubuntu-cloud-template"
   ip_address = "192.168.1.${151 + count.index}/24"
 }
+
+# Create 1 single Node cluster lab
+module "workers" {
+  source      = "./modules/proxmox-vm"
+  count       = 1
+  vm_name     = "k8s-worker-${count.index}"
+  vmid        = 600 + count.index
+  cores       = 4
+  memory      = 2048
+  ssh_key     = var.ssh_public_key
+  disk_size   = 20
+  storage_pool = "datastoreHDD1"
+  template_name = "ubuntu-cloud-template"
+  ip_address = "192.168.1.${151 + count.index}/24"
+}
